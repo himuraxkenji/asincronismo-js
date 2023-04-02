1d9a402b59f691fb23572854b5952fe750c74e5c
himuraxkenji <aaron.ariperto@gmail.com>
2023-04-01 23:23:06 -0300

    add generators

---
+++ src/generators/index.js
@@ -0,0 +1,30 @@
+function* gen() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+
+const g = gen()
+
+console.log(g.next().value);
+console.log(g.next().value);
+console.log(g.next().value);
+
+
+function* iterate(array){
+    for(let value of array){
+        yield value
+    }
+}
+
+
+const it = iterate(['himura', 'kenji', 'yen', 'dan'])
+
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
